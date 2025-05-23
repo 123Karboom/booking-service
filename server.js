a2638ef51f7c08269d82128cfa0af91add47a3ec
Le Hieu Nghia <lehieunghia2402@gmail.com>
2025-05-23 15:05:49 +0700

    change port 22

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.use('/api/categories', categoryRoutes);
 // Khởi tạo Swagger
 swaggerDocs(app);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
